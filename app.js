2641dd28bf1d271b6e93baeb66220e2313189ba4
Brandon Nguyen <lrbn86@gmail.com>
2019-08-07 19:52:17 -0400

    create a modal that handles user login

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const messageInput = document.querySelector("#messageInput");
 const messageButton = document.querySelector("#messageButton");
 const messagesBox = document.querySelector("#messagesBox");
+const loginButton = document.querySelector("#loginButton");
+// const loginModal = document.querySelector("#loginModal");
+
+var username = "";
 
 const sendMessage = function() {
-  const username = "lrbn86: ";
   const message = document.createElement("p");
-  message.textContent = username + messageInput.value;
+  message.textContent = username + ": " + messageInput.value;
   messagesBox.appendChild(message);
 
   // Receiving a new message will scroll to the latest message
   // even during scrolling.
   messagesBox.scrollTop = messagesBox.scrollHeight;
+}
+
+
+const loginUser = function() {
+  const usernameInput = document.querySelector("#usernameInput");
+  username = usernameInput.value;
 }
 
 messageInput.addEventListener('keypress', function(ev) {
@@ -23,4 +32,10 @@ messageInput.addEventListener('keypress', function(ev) {
 messageButton.addEventListener('click', function() {
   sendMessage();
   messageInput.value = '';
+});
+
+loginButton.addEventListener('click', function() {
+  loginUser();
+  const loginModal = document.getElementById("loginModal");
+  loginModal.style.display = "none";
 });
